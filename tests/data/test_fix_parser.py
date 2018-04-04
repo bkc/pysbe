8b623031ec75088190592bcecdb19bd94b029a0f
bkc <bkc@murkworks.com>
2018-04-03 22:38:15 -0400

    add test for malformed xml file

--- tests/data/test_fix_parser.py
+++ tests/data/test_fix_parser.py
@@ -1,9 +1,22 @@
 """test_fix_parser.py - test fix_parser"""
 import os
+import pytest
 
 from pysbe.parser.fix_parser import SBESpecParser
 
 class TestFixParser:
+
+    def test_parse_invalid_xml(self, test_data_dir, filename='invalid_sample1.xml'):
+        """parse this xml file"""
+        sbe = SBESpecParser()
+        with pytest.raises(ValueError):
+            sbe.parseFile(
+                os.path.join(
+                    test_data_dir,
+                    filename,
+                )
+            )
+
 
     def test_parse_xml(self, test_data_dir, filename='basic_sample1.xml'):
         """parse this xml file"""
